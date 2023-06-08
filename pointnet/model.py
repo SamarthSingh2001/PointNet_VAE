4e8381bcb39be6289990bc2aef285c65e1026a9c
SamarthSingh2001 <63026163+SamarthSingh2001@users.noreply.github.com>
2023-06-07 21:23:26 -0700

    Update model.py

--- pointnet/model.py
+++ pointnet/model.py
@@ -32,6 +32,7 @@ class STN3d(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         x = torch.max(x, 2, keepdim=True)[0]
+        #print("x shape", x.shape);
         x = x.view(-1, 1024)
 
         x = F.relu(self.bn4(self.fc1(x)))
@@ -39,9 +40,12 @@ class STN3d(nn.Module):
         x = self.fc3(x)
 
         iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
+        #print(iden)
         if x.is_cuda:
             iden = iden.cuda()
+       # print(x)
         x = x + iden
+        #print(x)
         x = x.view(-1, 3, 3)
         return x
 
@@ -105,6 +109,7 @@ class PointNetfeat(nn.Module):
         x = x.transpose(2, 1)
         x = torch.bmm(x, trans)
         x = x.transpose(2, 1)
+        #print(x.size())
         x = F.relu(self.bn1(self.conv1(x)))
 
         if self.feature_transform:
@@ -118,8 +123,11 @@ class PointNetfeat(nn.Module):
         pointfeat = x
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.bn3(self.conv3(x))
+        #print("x shape conv3", x.shape);
         x = torch.max(x, 2, keepdim=True)[0]
-        x = x.view(-1, 1024)
+        #print("x shape max", x.shape);
+        x = x.view(-1, 1024);
+        #print("x shape view", x.shape);
         if self.global_feat:
             return x, trans, trans_feat
         else:
@@ -173,7 +181,97 @@ class PointNetDenseCls(nn.Module):
         x = F.log_softmax(x.view(-1,self.k), dim=-1)
         x = x.view(batchsize, n_pts, self.k)
         return x, trans, trans_feat
+    
+class Encoder(nn.Module):
+    def __init__(self, k, feature_transform=False):
+        super(Encoder, self).__init__()
+        self.feature_transform = feature_transform
+        self.feat = PointNetfeat(global_feat=True, feature_transform=feature_transform)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k)
+        self.fc4 = nn.Linear(256, k)
+        self.dropout = nn.Dropout(p=0.3)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
 
+        self.N = torch.distributions.Normal(0, 1)
+        self.kl = 0
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x, trans, trans_feat = self.feat(x)
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.dropout(self.fc2(x))))
+        mu = self.fc3(x)
+        sigma = torch.exp(self.fc4(x))
+        if x.is_cuda:
+            mu = mu.cuda()
+            sigma = sigma.cuda()
+            sigma_ = self.N.sample(mu.shape)
+            sigma_ = sigma_.cuda()
+            sigma_ = sigma*sigma_
+        z = mu + sigma_
+        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        return z
+    
+class Decoder(nn.Module):
+    def __init__(self, k, feature_transform=False):
+        super(Decoder, self).__init__()
+        #self.k = k
+        self.stn = STN3d()
+        
+        self.linear1 = nn.Linear(16,256)
+        self.linear2 = nn.Linear(256,512)
+        self.linear3 = nn.Linear(512,1024)
+        self.linear4 = nn.Linear(1024,2048)
+        self.linear5 = nn.Linear(2048,4096)
+        self.linear6 = nn.Linear(4096,7500)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.bn2 = nn.BatchNorm1d(512)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(2048)
+        self.bn5 = nn.BatchNorm1d(4096)
+        self.drop1 = nn.Dropout(p=0.3)
+        
+        self.conv1 = torch.nn.Conv1d(3,64,1)
+        self.conv2 = torch.nn.Conv1d(5,4,1)
+        self.conv3 = torch.nn.Conv1d(4,3,1)
+        self.bn8 = nn.BatchNorm1d(64)
+        self.bn9 = nn.BatchNorm1d(4)
+              
+    def forward(self, x):
+        x = F.relu(self.bn1(self.drop1(self.linear1(x))))
+        x = F.relu(self.bn2(self.linear2(x)))
+        x = F.relu(self.bn3(self.linear3(x)))
+        x = F.relu(self.bn4(self.linear4(x)))
+        x = F.relu(self.bn5(self.linear5(x)))
+        x = F.relu(self.linear6(x))
+        #print(x.size())
+        x = x.reshape(x.size(0),3,2500)
+        #x = F.relu(self.bn8(self.conv1(x)))
+        #x = F.relu(self.conv3(x))
+        trans = self.stn(x)
+        #print(trans.size())
+        #print(x.size())
+        x = torch.bmm(trans, x)
+        x = torch.sigmoid(x)
+        #print(x.shape)
+        return x
+    
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(VariationalAutoencoder, self).__init__()
+        self.encoder = Encoder(latent_dims)
+        self.decoder = Decoder(latent_dims)
+
+    def forward(self, x):
+       # print(x.size())
+        z = self.encoder(x)
+        z = self.decoder(z)
+       # print(z.size())
+        return z
+        
 def feature_transform_regularizer(trans):
     d = trans.size()[1]
     batchsize = trans.size()[0]
